d679f2ee9844698be8fc106eba2c81cfb20cb9e7
svenosha <82269145+svenosha@users.noreply.github.com>
2021-05-17 01:22:56 +0800

    Design Manage Blood Donor Module

---
+++ app/src/main/java/com/example/bloodbank/UserBloodDonorList.java
@@ -0,0 +1,14 @@
+package com.example.bloodbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserBloodDonorList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_blood_donor_list);
+    }
+}

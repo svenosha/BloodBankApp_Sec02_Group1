483ae473437bc040c52ec5b013c8ca6e7f44c699
zul99hack <68143682+zul99hack@users.noreply.github.com>
2021-06-01 23:41:51 +0800

    request part

--- app/src/main/java/com/example/bloodbank/AdminHomepage.java
+++ app/src/main/java/com/example/bloodbank/AdminHomepage.java
@@ -2,13 +2,33 @@ package com.example.bloodbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminHomepage extends AppCompatActivity {
+
+    private Button btnrequestlist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_homepage);
+
+        btnrequestlist = findViewById(R.id.btn_viewrequests);
+
+        btnrequestlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRequestList();
+            }
+        });
+
+    }
+
+    public void openRequestList(){
+        Intent intentAdminRequestList = new Intent(this, AdminRequestList.class);
+        startActivity(intentAdminRequestList);
     }
 }
--- app/src/main/java/com/example/bloodbank/UserHomepage.java
+++ app/src/main/java/com/example/bloodbank/UserHomepage.java
@@ -2,13 +2,34 @@ package com.example.bloodbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserHomepage extends AppCompatActivity {
+
+    private Button btnMakeRequest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_homepage);
+
+        btnMakeRequest = findViewById(R.id.btn_makerequest);
+
+        btnMakeRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMakeRequest();
+            }
+        });
+
+    }
+
+    public void openMakeRequest(){
+        Intent intentMakeRequest = new Intent(this, UserMakeRequest.class);
+        startActivity(intentMakeRequest);
     }
 }

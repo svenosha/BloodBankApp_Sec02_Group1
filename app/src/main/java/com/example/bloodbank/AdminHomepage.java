9f0273c063f495d1ee325f3c9e110eee25d86da8
SafinaSahrawi <75608213+SafinaSahrawi@users.noreply.github.com>
2021-06-04 03:08:21 +0800

    Manage Account interfaces

--- app/src/main/java/com/example/bloodbank/AdminHomepage.java
+++ app/src/main/java/com/example/bloodbank/AdminHomepage.java
@@ -2,13 +2,56 @@ package com.example.bloodbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminHomepage extends AppCompatActivity {
+
+    private Button userList, donorList, requestList, bloodList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_homepage);
+
+        userList = findViewById(R.id.btn_viewusers);
+        donorList = findViewById(R.id.btn_viewdonors);
+        requestList = findViewById(R.id.btn_viewrequests);
+        bloodList = findViewById(R.id.btn_viewrequests);
+
+
+        userList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intenthome2user = new Intent (AdminHomepage.this, AdminUserList.class);
+                startActivity(intenthome2user);
+            }
+        });
+
+        donorList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent  intenthome2donor = new Intent (AdminHomepage.this, AdminBloodDonorList.class);
+                startActivity(intenthome2donor);
+            }
+        });
+
+        requestList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intenthome2request = new Intent (AdminHomepage.this, AdminUserList.class);
+//                startActivity(intenthome2request);
+            }
+        });
+
+        bloodList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intenthome2blood = new Intent (AdminHomepage.this, Adm.class);
+//                startActivity(intenthome2blood);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/bloodbank/UserHomepage.java
+++ app/src/main/java/com/example/bloodbank/UserHomepage.java
@@ -2,13 +2,58 @@ package com.example.bloodbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class UserHomepage extends AppCompatActivity {
+
+    private TextView username;
+    private Button profile, BDonor, BBank, request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_homepage);
+
+        setTitle("Home");
+
+        username = findViewById(R.id.tv_username);
+        profile = findViewById(R.id.btn_myprofile);
+        BDonor = findViewById(R.id.btn_blooddonor);
+        BBank = findViewById(R.id.btn_bloodbank);
+        request = findViewById(R.id.btn_makerequest);
+
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentHome2profile = new Intent (UserHomepage.this, UserViewProfile.class);
+                startActivity(intentHome2profile);
+            }
+        });
+
+        BDonor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        BBank.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }

58d69af77dfde0a8e6ff301dfc566132d58cc848
SafinaSahrawi <75608213+SafinaSahrawi@users.noreply.github.com>
2021-06-10 00:10:35 +0800

    Retrieve Data User

--- app/src/main/java/com/example/bloodbank/UserHomepage.java
+++ app/src/main/java/com/example/bloodbank/UserHomepage.java
@@ -1,5 +1,6 @@
 package com.example.bloodbank;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,10 +9,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
 public class UserHomepage extends AppCompatActivity {
 
     private TextView username;
     private Button profile, BDonor, BBank, btnMakeRequest;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +39,45 @@ public class UserHomepage extends AppCompatActivity {
         profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intentHome2profile = new Intent (UserHomepage.this, UserViewProfile.class);
-                startActivity(intentHome2profile);
+
+//                reference = FirebaseDatabase.getInstance().getReference().child("Member").child("1");
+
+                reference = FirebaseDatabase.getInstance().getReference().child("Member").child("1");
+                reference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+
+                            String namefromDB = snapshot.child("name").getValue().toString();
+                            String agefromDB = snapshot.child("age").getValue().toString();
+                            String genderfromDB = snapshot.child("gender").getValue().toString();
+                            String phonefromDB = snapshot.child("phone").getValue().toString();
+                            String emailfromDB = snapshot.child("email").getValue().toString();
+                            String locationfromDB = snapshot.child("location").getValue().toString();
+                            String usernamefromDB = snapshot.child("username").getValue().toString();
+                            String bloodtypefromDB = snapshot.child("bloodtype").getValue().toString();
+
+                            Intent intentHome2profile = new Intent(UserHomepage.this, UserViewProfile.class);
+
+                            intentHome2profile.putExtra("name", namefromDB);
+                            intentHome2profile.putExtra("age", agefromDB);
+                            intentHome2profile.putExtra("location", locationfromDB);
+                            intentHome2profile.putExtra("phone", phonefromDB);
+                            intentHome2profile.putExtra("email", emailfromDB);
+                            intentHome2profile.putExtra("username", usernamefromDB);
+                            intentHome2profile.putExtra("gender", genderfromDB);
+                            intentHome2profile.putExtra("bloodtype", bloodtypefromDB);
+
+                            startActivity(intentHome2profile);
+                        }
+
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
             }
         });
 

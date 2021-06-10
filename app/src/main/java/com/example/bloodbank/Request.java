b34cbed971f193fa7637f7c162b4ae2d6caffc77
zul99hack <68143682+zul99hack@users.noreply.github.com>
2021-06-10 09:00:14 +0800

    latest

---
+++ app/src/main/java/com/example/bloodbank/Request.java
@@ -0,0 +1,83 @@
+package com.example.bloodbank;
+
+public class Request {
+    private String Name;
+    private Integer Age;
+    private String Email;
+    private String Gender;
+    private String Phone;
+    private String Location;
+    private String BloodGroup;
+    private String Reason;
+
+    public Request() {
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Integer getAge() {
+        return Age;
+    }
+
+    public void setAge(Integer age) {
+        Age = age;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public String getBloodGroup() {
+        return BloodGroup;
+    }
+
+    public void setBloodGroup(String bloodGroup) {
+        BloodGroup = bloodGroup;
+    }
+
+    public String getReason() {
+        return Reason;
+    }
+
+    public void setReason(String reason) {
+        Reason = reason;
+    }
+
+    public String toString(){
+        return this.Name;
+    }
+}
